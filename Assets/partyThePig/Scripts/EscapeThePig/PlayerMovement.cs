d038795804f481c3abe442ab80cb867fe1aba5ca
Dai1234383 <tuwudade447@gmail.com>
2025-06-30 15:20:54 +0900

    Inputの実装、二人プレイの実装、タイトルから飛べるようにする

--- Assets/partyThePig/Scripts/EscapeThePig/PlayerMovement.cs
+++ Assets/partyThePig/Scripts/EscapeThePig/PlayerMovement.cs
@@ -1,41 +1,42 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+
+    [SerializeField] private int playerIndex; // 手動でインスペクターから設定
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private SpriteRenderer _spriteRenderer;    //画像
 
     private Vector2 moveInput;
     private Rigidbody2D rb;
 
-    private EscapeThePig controls;
+    private PlayerInput input;
 
-    void Awake()
+    private void Awake()
     {
-        controls = new EscapeThePig();
+        input = GetComponent<PlayerInput>();
+        playerIndex = input.playerIndex;  // 自動取得に変更
+
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void OnEnable()
-    {
-        controls.Enable();
-        controls.Player.Move.performed += OnMove;
-        controls.Player.Move.canceled += OnMove;
-    }
-
-    void OnDisable()
-    {
-        controls.Player.Move.performed -= OnMove;
-        controls.Player.Move.canceled -= OnMove;
-        controls.Disable();
-    }
-
-    private void OnMove(InputAction.CallbackContext context)
+    public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
     }
 
     void Start()
     {
+        if (PlayerManager.Instance != null && playerIndex >= 0 && playerIndex < PlayerManager.Instance.players.Length)
+        {
+            _spriteRenderer.color = PlayerManager.Instance.players[playerIndex].playerColor;
+        }
+        else
+        {
+            Debug.LogWarning($"PlayerManager が見つからないか、playerIndex が無効です: {playerIndex}");
+        }
         rb = GetComponent<Rigidbody2D>();
     }
 
